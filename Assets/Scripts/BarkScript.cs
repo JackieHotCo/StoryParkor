772c711c419f28f84467bcb322e100403579bf79
Jacqueline Christina Boesiger <jacqueline@bosiger.com>
2025-03-07 15:26:44 +0100

    Made dog a asset

---
+++ Assets/Scripts/BarkScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BarkScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+    }
+}
