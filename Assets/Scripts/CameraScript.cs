7ea4ae2a4b53edc82b32a40cb497958ace322eca
Jacqueline Christina Boesiger <jacqueline@bosiger.com>
2025-03-13 02:13:07 +0100

    Added camera movement and fixed jumping

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // The Speed in which the camera follows the player
+    public float FollowSpeed = 3f;
+    // How far on the x axis is the camera away from the player
+    public float xOffset = 10f;
+    // The y coordinates of the camera
+    public float yOffset = 10f;
+    // Location of the player
+    public Transform target;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Gets new location of the camera
+        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
+        // Changes the position of the camera to the new position
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
