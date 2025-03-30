c3bd95dc4452a8fef2ce571aaf4f2af4c10e8045
Jacqueline Christina Boesiger <jacqueline@bosiger.com>
2025-03-30 18:03:59 +0200

    Adding death box

--- Assets/Scripts/DeathBoxScript.cs
+++ Assets/Scripts/DeathBoxScript.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class DeathBoxScript : MonoBehaviour
 {
-    Vector2 startPosition;
+    public Vector2 startPosition;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("die");
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.transform.position = startPosition;
